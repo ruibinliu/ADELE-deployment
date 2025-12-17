bc352c309b5c9e095bad57f532e45e904a95f516
rbliu <rbliu@ieee.org>
2025-12-17 17:15:50 +0800

    Change domain from inesc-id to singou.net.

--- gdi-core/nginx/get_cert.sh
+++ gdi-core/nginx/get_cert.sh
@@ -4,10 +4,9 @@ docker compose run --rm certbot certonly \
     --webroot --webroot-path=/var/www/certbot \
     --agree-tos --no-eff-email \
     --cert-name adele-certs \
-    -d fdp.adele.inesc-id.pt -d rems.adele.inesc-id.pt \
-    -d inbox.adele.inesc-id.pt -d login.adele.inesc-id.pt \
-    -d download.adele.inesc-id.pt -d backoffice.adele.inesc-id.pt \
-    -d website.adele.inesc-id.pt -d website-api.adele.inesc-id.pt \
-    -d backoffice-api.adele.inesc-id.pt
-
+    -d fdp.tre.singou.net -d rems.tre.singou.net \
+    -d inbox.tre.singou.net -d login.tre.singou.net \
+    -d download.tre.singou.net -d backoffice.tre.singou.net \
+    -d website.tre.singou.net -d website-api.tre.singou.net \
+    -d backoffice-api.tre.singou.net
 
